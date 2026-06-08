89e5e95968fa3f36929527adb1b1d29c2a94521a
samycherch <202838912+samycherch@users.noreply.github.com>
2026-06-08 16:49:16 +0200

    Ajout du chargement des pages suivantes et précedentes

--- src/lib/gallery.ts
+++ src/lib/gallery.ts
@@ -23,13 +23,35 @@ export interface Gallery {
 
 let currentGallery: Gallery = { photos: [], links: {} };
 
-export async function load(): Promise<Gallery> {
-    const data = await loadResource<{ photos: { photo: GalleryPhoto }[]; links: GalleryLinks }>(
-        `${API_URL}/photos`
-    );
+async function loadGalleryFromUri(uri: string): Promise<Gallery> {
+    const data = await loadResource<{ photos: { photo: GalleryPhoto }[]; links: GalleryLinks }>(uri);
     currentGallery = {
         photos: data.photos.map(item => item.photo),
         links: data.links
     };
     return currentGallery;
+}
+
+export async function load(): Promise<Gallery> {
+    return loadGalleryFromUri(`${API_URL}/photos`);
+}
+
+export async function next(): Promise<Gallery> {
+    if (!currentGallery.links.next) throw new Error("Pas de page suivante.");
+    return loadGalleryFromUri(`https://webetu.iutnc.univ-lorraine.fr${currentGallery.links.next.href}`);
+}
+
+export async function prev(): Promise<Gallery> {
+    if (!currentGallery.links.prev) throw new Error("Pas de page précédente.");
+    return loadGalleryFromUri(`https://webetu.iutnc.univ-lorraine.fr${currentGallery.links.prev.href}`);
+}
+
+export async function first(): Promise<Gallery> {
+    if (!currentGallery.links.first) throw new Error("Pas de première page.");
+    return loadGalleryFromUri(`https://webetu.iutnc.univ-lorraine.fr${currentGallery.links.first.href}`);
+}
+
+export async function last(): Promise<Gallery> {
+    if (!currentGallery.links.last) throw new Error("Pas de dernière page.");
+    return loadGalleryFromUri(`https://webetu.iutnc.univ-lorraine.fr${currentGallery.links.last.href}`);
 }
